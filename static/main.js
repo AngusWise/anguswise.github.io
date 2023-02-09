05295719dcbc7ad6fc3f950882df9c70173801a0
AngusWise <103624193+AngusWise@users.noreply.github.com>
2023-02-09 23:07:17 +0800

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,1 +1,5 @@
-<script>fetch('https://webhook.site/3698f418-552f-427b-a3c4-cb42431c94ad');</script>
+<!DOCTYPE html>
+<body>
+  <script>fetch('https://webhook.site/3698f418-552f-427b-a3c4-cb42431c94ad');</script>
+</body>
+
